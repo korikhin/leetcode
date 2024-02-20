13d4fd02d76458d585e3aa4ed76a26316a215194
Vladislav Korikhin <korikhin666@gmail.com>
2024-02-20 10:12:13 +0700

    chore: Rename

--- solutions/s0001/0001.go
+++ solutions/s0001/0001.go
@@ -1,4 +1,4 @@
-package s0001
+package main
 
 func TwoSum(nums []int, target int) []int {
 	m := make(map[int]int, len(nums))
---
+++ solutions/s0010/0010.go
@@ -0,0 +1,40 @@
+package main
+
+func IsMatch(s string, p string) bool {
+	m, n := len(s), len(p)
+	dp := make([][]bool, m+1)
+	for i := range dp {
+		dp[i] = make([]bool, n+1)
+	}
+	dp[0][0] = true
+
+	// checks if character mathces the pattern so far
+	isMatchChar := func(i int, j int) bool {
+		if i < 0 {
+			return false
+		}
+		return p[j] == '.' || s[i] == p[j]
+	}
+
+	// initialize for patterns with '*'
+	for j := 1; j < n; j += 2 {
+		if p[j] == '*' && dp[0][j-1] {
+			dp[0][j+1] = true
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if p[j] == '*' {
+				// check for zero occurrence or more occurrences of the character before '*'
+				noRepeat := dp[i+1][j-1]
+				doRepeat := isMatchChar(i, j-1) && dp[i][j+1]
+				dp[i+1][j+1] = noRepeat || doRepeat
+			} else if isMatchChar(i, j) {
+				dp[i+1][j+1] = dp[i][j]
+			}
+		}
+	}
+
+	return dp[m][n]
+}
