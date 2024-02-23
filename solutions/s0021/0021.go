d75c59d1191699227362f0d3c52900756826f88d
Vladislav Korikhin <korikhin666@gmail.com>
2024-02-24 05:10:23 +0700

    21

---
+++ solutions/s0021/0021.go
@@ -0,0 +1,31 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	carry := dummy
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			carry.Next = list1
+			list1 = list1.Next
+		} else {
+			carry.Next = list2
+			list2 = list2.Next
+		}
+		carry = carry.Next
+	}
+
+	if list1 != nil {
+		carry.Next = list2
+	} else if list2 == nil {
+		carry.Next = list1
+	}
+
+	return dummy.Next
+}
