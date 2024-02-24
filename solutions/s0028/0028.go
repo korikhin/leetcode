c66c0d702380f133c29cafc9970de686197fcf83
Vladislav Korikhin <korikhin666@gmail.com>
2024-02-25 03:41:39 +0700

    28

---
+++ solutions/s0028/0028.go
@@ -0,0 +1,29 @@
+package main
+
+func StrStr(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	if len(needle) > len(haystack) {
+		return -1
+	}
+
+	haystackRunes := []rune(haystack)
+	needleRunes := []rune(needle)
+
+	for i := 0; i <= len(haystackRunes)-len(needleRunes); i++ {
+		matchFound := true
+		for j := 0; j < len(needleRunes); j++ {
+			if haystackRunes[i+j] != needleRunes[j] {
+				matchFound = false
+				break
+			}
+		}
+
+		if matchFound {
+			return i
+		}
+	}
+
+	return -1
+}
