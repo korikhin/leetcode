ce42d75b092037ed422dea17c305b3b9dd901c43
Vladislav Korikhin <korikhin666@gmail.com>
2024-03-02 15:16:10 +0700

    40

---
+++ solutions/s0040/0040.go
@@ -0,0 +1,32 @@
+package main
+
+import "sort"
+
+func CombinationSum(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+
+	var (
+		result [][]int
+		crunch func(int, int, []int)
+	)
+
+	crunch = func(start, target int, combination []int) {
+		if target == 0 {
+			result = append(result, append([]int(nil), combination...))
+			return
+		}
+
+		for i := start; i < len(candidates); i++ {
+			if candidates[i] > target {
+				break
+			}
+			if i > 0 && candidates[i] == candidates[i-1] {
+				continue
+			}
+			crunch(i+1, target-candidates[i], append(combination, candidates[i]))
+		}
+	}
+
+	crunch(0, target, nil)
+	return result
+}
