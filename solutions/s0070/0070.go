fb9fa70ae650765ba3be83409549140152ff493c
Vladislav Korikhin <korikhin666@gmail.com>
2024-03-13 17:17:33 +0700

    70

---
+++ solutions/s0070/0070.go
@@ -0,0 +1,17 @@
+package main
+
+func ClimbStairs(n int) int {
+	if n < 0 {
+		return 1
+	}
+	if n < 3 {
+		return n
+	}
+
+	tic, tac := 1, 2
+	for i := 2; i < n; i++ {
+		tic, tac = tac, tic+tac
+	}
+
+	return tac
+}
