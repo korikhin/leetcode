f47d30be860dc089459783d3124f0129bd53c46d
Vladislav Korikhin <korikhin666@gmail.com>
2024-04-12 15:57:30 +0700

    1

--- solutions/s0001/0001.go
+++ solutions/s0001/0001.go
@@ -1,15 +1,10 @@
 package main
 
 func TwoSum(nums []int, target int) []int {
-	n := len(nums)
-	if n < 2 {
-		return nil
-	}
+	m := make(map[int]int, len(nums))
 
-	m := make(map[int]int, n)
-
-	for i, num := range nums {
-		if j, ok := m[target-num]; ok {
+	for i, n := range nums {
+		if j, ok := m[target-n]; ok {
 			return []int{j, i}
 		}
 		m[n] = i
