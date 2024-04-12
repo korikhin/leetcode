cb7cd23f5e72b31f1eb5ab4b20f88ee7c2a70301
Vladislav Korikhin <korikhin666@gmail.com>
2024-04-12 15:36:02 +0700

    167, 392

---
+++ solutions/s0392/0392.go
@@ -0,0 +1,18 @@
+package main
+
+func IsSubsequence(s string, t string) bool {
+	if len(s) > len(t) {
+		return false
+	}
+
+	for i, k := 0, 0; k < len(t); k++ {
+		if s[i] == t[k] {
+			i++
+		}
+		if i == len(s) {
+			return true
+		}
+	}
+
+	return false
+}
