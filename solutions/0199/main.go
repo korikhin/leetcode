d121b7801f2c36a8a2b09da41d7ff808d0a83579
Vladislav Korikhin <korikhin666@gmail.com>
2025-02-10 00:50:24 +0700

    199

---
+++ solutions/0199/main.go
@@ -0,0 +1,37 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	var rightSide []int
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		for i := 0; i < levelSize; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			if i == 0 {
+				rightSide = append(rightSide, node.Val)
+			}
+
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+		}
+	}
+
+	return rightSide
+}
