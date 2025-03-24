bb63fa694edd26d02535fc97f25df2f3fdbc825b
Vladislav Korikhin <korikhin666@gmail.com>
2025-03-24 14:02:29 +0700

    177: format

--- solutions/0177/pg.sql
+++ solutions/0177/pg.sql
@@ -1,19 +1,19 @@
-create or replace function NthHighestSalary (n int)
-    returns table (
-        salary int
-    )
-    as $$
-begin
-    if n < 1 then
-        return;
-    end if;
+CREATE OR REPLACE FUNCTION NthHighestSalary (n integer)
+RETURNS TABLE (
+    salary integer
+)
+AS $$
+BEGIN
+    IF n IS NULL OR n < 1 THEN
+        RETURN;
+    END IF;
 
-    return query (
-        select distinct(e.salary)
-        from public.Employee as e
-        order by e.salary desc
-        limit 1
-        offset n - 1
+    RETURN QUERY (
+        SELECT DISTINCT(e.salary)
+        FROM public.Employee AS e
+        ORDER BY e.salary DESC
+        LIMIT 1
+        OFFSET n - 1
     );
-end;
-$$ language plpgsql;
+END;
+$$ LANGUAGE plpgsql;
